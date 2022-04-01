6f0d5eb15d9fe9cb3659264620b823cbb71b4d2b
Jawad Chowdhury <jawadc13@gmail.com>
2022-04-01 09:24:11 -0500

    adding backend and figuring out docker compose more

--- angular-ecommerce/src/environments/environment.prod.ts
+++ angular-ecommerce/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  ecommerceUrl: "https://localhost:8080",
+  stripePublishableKey: "pk_test_51KfpTVAVStLNAELTHj4bFvix6sySvR19arB8g1xadof7eeWk2RqA9NOalOo4SNJtb5P1zCz5CbYVFDLPxF5X34S000hoxBuUmL"
 };
